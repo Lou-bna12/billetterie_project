c465eb2c4b321e16303f068a5b636a0f8ad89998
Loubna Sellam <loubnasellam49@gmail.com>
2025-06-15 18:37:14 +0200

    GIT COMMIT

--- billetterie/urls.py
+++ billetterie/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register/', views.inscription, name='inscription'),
     path('login/', views.connexion, name='connexion'),
     path('logout/', views.deconnexion, name='deconnexion'),
-    path('offres/', views.liste_offres, name='offres')
+    path('offres/', views.liste_offres, name='offres'),
+    path('ajouter-au-panier/<int:offre_id>/', views.ajouter_au_panier, name='ajouter_au_panier'),
 ]
--- billetterie/views.py
+++ billetterie/views.py
@@ -1,21 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.contrib import messages
-from .models import Offre
 
-def liste_offres(request):
-    offres = Offre.objects.all()
-    return render(request, 'billetterie/offres.html', {'offres': offres})
+from .models import Utilisateur, Offre, Panier, PanierOffre
 
 
-
-# Vue de la page d'accueil
 def accueil(request):
     return render(request, 'billetterie/accueil.html')
 
 
-# Vue de la page d'inscription
 def inscription(request):
     if request.method == "POST":
         nom = request.POST.get("nom")
@@ -33,12 +27,20 @@ def inscription(request):
 
         user = User.objects.create_user(username=email, email=email, password=mot_de_passe, first_name=nom)
         user.save()
+
+        utilisateur = Utilisateur.objects.create(
+            nom=nom,
+            prenom="",
+            email=email,
+            mot_de_passe=mot_de_passe  # À sécuriser plus tard
+        )
+        utilisateur.save()
+
         return redirect('connexion')
 
     return render(request, 'billetterie/inscription.html')
 
 
-# Vue de la connexion
 def connexion(request):
     if request.method == "POST":
         email = request.POST.get("email")
@@ -55,7 +57,31 @@ def connexion(request):
     return render(request, 'billetterie/connexion.html')
 
 
-# Vue de la déconnexion
 def deconnexion(request):
     logout(request)
     return redirect('connexion')
+
+
+def liste_offres(request):
+    offres = Offre.objects.all()
+    return render(request, 'billetterie/offres.html', {'offres': offres})
+
+
+def ajouter_au_panier(request, offre_id):
+    if not request.user.is_authenticated:
+        return redirect('connexion')
+
+    utilisateur = Utilisateur.objects.get(email=request.user.username)
+    offre = get_object_or_404(Offre, id=offre_id)
+
+    panier, _ = Panier.objects.get_or_create(utilisateur=utilisateur)
+
+    item, created = PanierOffre.objects.get_or_create(
+        panier=panier,
+        offre=offre,
+    )
+    if not created:
+        item.quantite += 1
+        item.save()
+
+    return redirect('offres')
